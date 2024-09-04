7ef8505de54bbb9d5770555ea162d3119182e8aa
Toby Ralph <ybothd@gmail.com>
2024-09-04 16:13:54 +0100

    Edits to classes

--- __init__.py
+++ __init__.py
@@ -1,7 +1,14 @@
-class Roof:
-    def __init__(self, span, length, bay_size, beam_type, pitch, build_method):
-        self.span = span
+class Rect_building:
+    def __init__(self, width, length, height):
+        self.width = width
         self.length = length
+        self.height = height
+
+class Duopitch:
+    def __init__(self, building, bay_size, beam_type, pitch, build_method):
+        self.building = building
+        self.span = building.width
+        self.length = building.length
         self.bay_size = bay_size
         self.beam = beam_type
         self.pitch = pitch
@@ -13,11 +20,15 @@ class Roof:
         return area
 
     def parts_list(self):
-        parts = []
+        parts = [['LFX0001', 4*6],
+                 ['LVS0001', 4]]
         # Logic to determine required parts based on variables
-        # ...
         return parts
     
-my_roof = Roof(20, 40, 2.5, "LV78", 18, "Hand")
+
+my_building = Rect_building(20, 40, 3)
+my_roof = Duopitch(my_building, 2.5, "LV78", 18, "Hand")
 integrity = my_roof.structural_integrity()
+parts = my_roof.parts_list()
 print(integrity)
+print(parts)
