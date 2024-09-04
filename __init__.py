3952f8f158b3928f58f82532b75c48e4a2f69c35
Toby Ralph <ybothd@gmail.com>
2024-09-04 17:02:01 +0100

    parts.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,14 @@
 import math
+import parts
 
 class Duopitch:
-    def __init__(self, lhs, rhs, length, bay_size, beam_type, pitch, build_method):
+    def __init__(self, lhs:int, rhs:int, length:float, bay_size:float, pitch:int, beam_type:str):
         self.lhs = lhs + 0.5
         self.rhs = rhs + 0.5
         self.length = length
         self.bay_size = bay_size
         self.beam = beam_type
         self.pitch = pitch
-        self.method = build_method
         self.span = math.cos(math.radians(self.pitch)) * (self.lhs + self.rhs)
 
     def structural_integrity(self):
@@ -16,15 +16,15 @@ class Duopitch:
         area = self.span * self.length 
         return area
 
-    def parts_list(self):
+    def parts_list(self, build_method:str):
         parts = [['LFX0001', 4*6],
                  ['LVS0001', 4]]
         # Logic to determine required parts based on variables
         return parts
 
 
-my_roof = Duopitch(10, 10, 40, 2.5, "LV78", 18, "Hand")
+my_roof = Duopitch(10, 10, 40, 2.5, 18, "LV78")
 integrity = my_roof.structural_integrity()
-parts = my_roof.parts_list()
+parts = my_roof.parts_list("Hand")
 print(integrity)
 print(parts)
---
+++ parts.py
@@ -0,0 +1,8 @@
+def beamline(lhs, rhs, beam_type: str, roof_type: str):
+    beamline_parts = []
+
+    if beam_type == "LV78":
+
+
+
+    return 1
